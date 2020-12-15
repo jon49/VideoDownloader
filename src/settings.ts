38a81426919d17866e7ef655a8ddfaf6754f1f90
Jon Nyman <nymanjon@gmail.com>
2020-12-14 21:08:20 -0700

    Hard code settings type

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,5 @@
+import { exists } from "https://deno.land/std/fs/mod.ts"
+
 export const appPath = Deno.env.get("USERPROFILE") + "/.video-downloader"
 export const binPath = appPath + "/bin"
 const settingsPath = appPath + "/settings.json"
@@ -5,9 +7,17 @@ export const dlPath = binPath + "/youtube-dl.exe"
 export const ffmpegPath = binPath + "/ffmpeg.exe"
 export const _7zPath = binPath + "/7z.exe"
 
-export const getData = async <T>() => {
+export interface HomeModel {
+    errors: string[] | undefined | null
+    format: "lqytmp3" | "hqytmp3" | "hqyt" | undefined
+    urls: (string | undefined | null)[]
+    targetDirectory: string | undefined
+}
+
+export const getData = async () : Promise<HomeModel> => {
+    if (!(await exists(settingsPath))) return {} as HomeModel
     const text = await Deno.readTextFile(settingsPath)
-    return <T>JSON.parse(text)
+    return JSON.parse(text)
 }
 
 export const setData = <T>(data: T) =>
