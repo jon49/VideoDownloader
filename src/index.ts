cc962e03052fa8213396b4aa2e7e65ad4b1a3cf6
Jon Nyman <nymanjon@gmail.com>
2020-12-14 21:07:51 -0700

    Added youtube updater on start of application

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { serve } from "https://deno.land/std@0.79.0/http/server.ts";
 import router from "./router.ts"
+import { dlPath } from "./settings.ts"
+import { exists } from "https://deno.land/std/fs/mod.ts"
 
 // deno run --allow-net --allow-run .\src\index.ts
 
@@ -9,6 +11,12 @@ const url = `http://localhost:${port}/`
 
 console.log(url)
 Deno.run({cmd: ["explorer", url]})
+exists(dlPath)
+.then(x => {
+  if (x) {
+    Deno.run({cmd: [dlPath, "-U"]})
+  }
+})
 
 const start = async () => {
   for await (const req of server) {
